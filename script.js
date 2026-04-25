0756df7bab12638c8f5d3d5e2c1de21c87f44f61
ADHETINSER <ab.adhetinser@gmail.com>
2026-04-25 14:45:45 -0600

    Add toggleMenu and showPage functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function toggleMenu() {
+  document.getElementById("menu").classList.toggle("active");
+}
 
+function showPage(pageId) {
+
+  // Oculta todas las páginas
+  document.querySelectorAll(".page").forEach(page => {
+    page.classList.remove("active");
+  });
+
+  // Muestra la seleccionada
+  document.getElementById(pageId).classList.add("active");
+
+  // Cierra el menú
+  document.getElementById("menu").classList.remove("active");
+
+  // Scroll arriba
+  window.scrollTo(0, 0);
+}
